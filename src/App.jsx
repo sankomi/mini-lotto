5321b66c1b490bad140d2f3de1aba6bdfd07620a
sanko <sang.koh@hotmail.com>
2023-12-01 19:29:41 +1100

    display results

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 
 export default function App() {
-	return <h1>mini lotto</h1>;
+	function drawNumbers(start, end, n) {
+		let pool = [];
+		for (let i = start; i <= end; i++) {
+			pool.push(i);
+		}
+
+		let drawn = [];
+		for (let i = 0; i < n; i++) {
+			let index = Math.floor(Math.random() * pool.length);
+			drawn.push(pool[index]);
+			pool.splice(index, 1);
+		}
+
+		return drawn;
+	}
+
+	return <>
+		<h1>mini lotto</h1>
+		<LottoResult result={drawNumbers(1, 20, 4)}/>
+	</>;
 }
 
-function drawNumbers(start, end, n) {
-	let pool = [];
-	for (let i = start; i <= end; i++) {
-		pool.push(i);
-	}
+function LottoResult({result}) {
+	return <div className="results">
+		{result.map((number, index) => <LottoBall key={index} value={number}/>)}
+	</div>;
+}
 
-	let drawn = [];
-	for (let i = 0; i < n; i++) {
-		let index = Math.floor(Math.random() * pool.length);
-		drawn.push(pool[index]);
-		pool.splice(index, 1);
-	}
-
-	return drawn;
+function LottoBall({value}) {
+	return <div className="ball">{value}</div>;
 }
