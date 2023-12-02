92e3341b60d0ae29bf1e238b7233d16be7f98e25
sanko <sang.koh@hotmail.com>
2023-12-02 13:02:57 +1100

    add random tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function App() {
+	const [drawNumber, setDrawNumber] = useState(1);
+	const [results, setResults] = useState([]);
+	const [tickets, setTickets] = useState([]);
+
 	function drawNumbers(start, end, n) {
 		let pool = [];
 		for (let i = start; i <= end; i++) {
@@ -14,19 +18,68 @@ export default function App() {
 			pool.splice(index, 1);
 		}
 
-		return drawn;
+		setDrawNumber(drawNumber + 1);
+		setResults([...results, {drawNumber, numbers: drawn}]);
+	}
+
+	function addRandomTicket(drawNumber, start, end, n) {
+		let ticket = {
+			drawNumber,
+			numbers: [],
+		};
+
+		let pool = [];
+		for (let i = start; i <= end; i++) {
+			pool.push(i);
+		}
+
+		for (let i = 0; i < n; i++) {
+			let index = Math.floor(Math.random() * pool.length);
+			ticket.numbers.push(pool[index]);
+			pool.splice(index, 1);
+		}
+
+		setTickets([...tickets, ticket]);
 	}
 
 	return <>
 		<h1>mini lotto</h1>
-		<LottoResult result={drawNumbers(1, 20, 4)}/>
+		<button onClick={() => drawNumbers(1, 20, 4)}>check</button>
+		<LottoResult result={results.length > 0? results[results.length - 1]: null}/>
+		<TicketList tickets={tickets}/>
+		<button onClick={() => addRandomTicket(drawNumber, 1, 20, 4)}>add</button>
 	</>;
 }
 
+function TicketList({tickets}) {
+	return <>
+		<h2>tickets</h2>
+		<ul className="tickets">
+			{tickets.map((ticket, index) => <TicketItem key={index} ticket={ticket}/>)}
+		</ul>
+	</>;
+}
+
+function TicketItem({ticket}) {
+	return <li className="ticket">
+		<div>draw number {ticket.drawNumber}</div>
+		<div className="balls">
+			{ticket.numbers.map((number, index) => <LottoBall key={index} value={number}/>)}
+		</div>
+	</li>;
+}
+
 function LottoResult({result}) {
-	return <div className="results">
-		{result.map((number, index) => <LottoBall key={index} value={number}/>)}
-	</div>;
+	if (!result) {
+		return <></>;
+	}
+
+	return <>
+		<div>draw number {result.drawNumber}</div>
+		<div className="results balls">
+			{result.numbers.map((number, index) => <LottoBall key={index} value={number}/>)}
+		</div>
+	</>;
 }
 
 function LottoBall({value}) {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import "./style.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
